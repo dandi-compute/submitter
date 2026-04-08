fac6dde824a5a1f96f6acc79ce499f4b954e2b3b
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-04-08 15:24:02 -0400

    Add launch_runner.sh for job submission setup

---
+++ launch_runner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=DANDI-Compute-Listener
+#SBATCH --mem=1GB
+#SBATCH --cpus-per-task 1
+#SBATCH --partition=mit_preemptable 
+#SBATCH --time=12:00:00
+
+source /etc/profile.d/modules.sh
+module load miniforge
+conda activate /orcd/data/dandi/001/environments/name-dandi+compute_env
+flock -n /orcd/data/dandi/001/dandi-compute/submitter/submitter.lock -c "/orcd/data/dandi/001/dandi-compute/submitter/actions-runner/run.sh" || echo "$(date): lock held, skipping submit"
