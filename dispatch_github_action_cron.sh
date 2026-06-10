822643dfcd7105f77996d2a6b7e769eec6886aee
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-06-10 13:00:39 -0400

    Fix URL for GitHub Actions workflow dispatch

--- dispatch_github_action_cron.sh
+++ dispatch_github_action_cron.sh
@@ -6,5 +6,5 @@ curl -fsS -X POST \
   -H "Authorization: Bearer $GH_TOKEN" \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/dandi-compute/submitter/actions/workflows/process_queue.yml/dispatches \
+  https://api.github.com/repos/dandi-compute/submitter/actions/workflows/process-queue.yml/dispatches \
   -d '{"ref":"main"}'
